196aafa7b20b70b6637505139a16f10a08f80fde
rakeshsagar <rakesh.sagar@yahoo.com>
2015-02-12 14:41:44 +0530

    Test

--- Systems/src/main/java/rakesh/Systems/App.java
+++ Systems/src/main/java/rakesh/Systems/App.java
@@ -2,15 +2,12 @@ package rakesh.Systems;
 
 /**
  * Hello world!
- * 
+ *
  */
-public class App
+public class App 
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello UIM 4 storage");
-        System.out.println("Hello UIM4 System");
+        System.out.println( "Hello World!" );
     }
 }
