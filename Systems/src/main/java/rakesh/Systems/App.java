62ecb08510969938a8490f1dd77bcd6ab50743a7
rakeshsagar <rakesh.sagar@yahoo.com>
2015-02-12 12:26:27 +0530

    Conflict

--- Systems/src/main/java/rakesh/Systems/App.java
+++ Systems/src/main/java/rakesh/Systems/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println("Hello UIM4Z");
+
     }
 }
