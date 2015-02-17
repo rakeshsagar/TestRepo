6e84332fb7abf635b0a2d94e1ecb51ce7cf4eb07
rakeshsagar <rakesh.sagar@yahoo.com>
2015-02-17 10:44:42 +0530

    Final Merge

--- Systems/src/main/java/rakesh/Systems/App.java
+++ Systems/src/main/java/rakesh/Systems/App.java
@@ -9,8 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("from dev");
-        System.out.println("from Rakesh Dev");
-        System.out.println("Again from Rakesh Dev");
     }
 }
